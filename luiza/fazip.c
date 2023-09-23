f05d0528ce0b9b756c707ea9da135138d45d4c0f
LuizaBB <luizabezerrabastos@gmail.com>
2023-09-23 02:20:54 -0300

    mudança para lista encadeada com prioridade

--- luiza/fazip.c
+++ luiza/fazip.c
@@ -8,67 +8,31 @@ struct node
     unsigned char byte;
     int frequency;
     struct node *next;
-    // struct node *right;
-    // struct node *left;
+    struct node *left;
+    struct node *right;
 };
-struct priority_queue
+bool vazio(struct node *fila)
 {
-    struct node *head;
-};
-bool vazio(struct priority_queue *fila)
-{
-    if(fila->head==NULL)
+    if(fila==NULL)
     {return true;}
     return false;
 }
-void printar_fila(struct priority_queue *fila)
+void printar_fila(struct node *fila)
 {
-    while(fila->head!=NULL)
-    {   printf("Byte numero: %d, Frequencia: %d\n", fila->head->byte, fila->head->frequency);
-        fila->head=fila->head->next;}
+    while(vazio(fila)==false)
+    {   printf("Byte %c numero: %d, Frequencia: %d\n", fila->byte, fila->byte, fila->frequency);
+        fila=fila->next;}
     return;
 }
-void adicionar_fila(struct priority_queue *fila, int item, int frequencia)
+void printar_preordem(struct node *fila)
 {
-    struct node *novo_no=malloc(sizeof(struct node));
-    novo_no->byte=item;
-    novo_no->frequency=frequencia;
-    if(vazio(fila)==true|| frequencia < fila->head->frequency)
-    {    novo_no->next=fila->head;
-        fila->head=novo_no;}
-    else
+    while(vazio(fila)==false)
     {
-        struct node *atual=fila->head;
-        while(atual->next!=NULL && frequencia > atual->next->frequency)
-        {atual= atual->next;}
-        novo_no->next=atual->next;
-        atual->next=novo_no;
+        printf("byte %c ", fila->byte);
+        printar_preordem(fila->left);
+        printar_preordem(fila->right);
     }
-    return;
-}
-struct priority_queue *filtrar_array_filaprioridade(char array[], struct priority_queue *fila)
-{
-    for(int i=0;i<MAX_BYTES;i++)
-    {   if(array[i]>0)
-        {adicionar_fila(fila, i, array[i]);}}
-    return fila;
-}
-struct priority_queue *criar_filaprioridade()
-{
-    struct priority_queue *nova=malloc(sizeof(struct priority_queue));
-    nova->head=NULL;
-    return nova;
-}
-void pegando_frequencias(char array[], char nome[])
-{
-    unsigned char caracter;
-    strcat(nome, ".txt");
-    FILE *file=fopen(nome, "rb");
-    while(fread(&caracter, sizeof(char), 1,file)>0)
-    {array[caracter]=array[caracter]+1;
-    printf("caracter:%c %d\n",caracter, caracter);}
-    fclose(file);
-    return;
+    printf("\n");
 }
 void iniciando_array(char array[])
 {
@@ -76,6 +40,25 @@ void iniciando_array(char array[])
     {array[i]=0;}
     return;
 }
+// struct priority_queue *criar_filaprioridade()
+// {
+//     struct priority_queue *nova=malloc(sizeof(struct priority_queue));
+//     nova->head=NULL;
+//     return nova;
+// }
+void pegando_frequencias(char array[], char nome[])
+{
+    unsigned char caracter;
+    strcat(nome, ".txt");
+    FILE *file=fopen(nome, "rb");
+    while(fread(&caracter, sizeof(char), 1,file)>0)
+    {array[caracter]=array[caracter]+1;
+    // printf("caracter:%c %d\n",caracter, caracter);
+    }
+    fclose(file);
+    return;
+}
+
 int main()
 {
     char todosbytes[MAX_BYTES];
@@ -84,8 +67,9 @@ int main()
     printf("Inserir nome do arquivo: ");
     scanf("%s", nome_arquivo);
     pegando_frequencias(todosbytes, nome_arquivo);//checado
-    struct priority_queue *filabytes=criar_filaprioridade();
+    struct node *filabytes=NULL;
     filabytes=filtrar_array_filaprioridade(todosbytes,filabytes);
-    printar_fila(filabytes);
+    printar_fila(filabytes);//checado
+    
     return 0;
 }
