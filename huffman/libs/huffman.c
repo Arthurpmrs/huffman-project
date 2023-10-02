ab79eba1e3e4c192a58ed8900d5058af2bcb9947
Arthurpmrs <arthurpmrs@gmail.com>
2023-10-02 17:39:08 -0300

    refactor huffman.c

--- huffman/libs/huffman.c
+++ huffman/libs/huffman.c
@@ -74,45 +74,53 @@ huff_node_t *huff_dequeue(huff_queue_t *hq)
 
 huff_node_t *huff_get_tree_from_queue(huff_queue_t *hq)
 {
-    if(hq->size == 1){
-        huff_node_t* node = huff_dequeue(hq);
-        huff_node_t* root = malloc(sizeof(huff_node_t));
+    if (hq->size == 1)
+    {
+        huff_node_t *node = huff_dequeue(hq);
+        huff_node_t *root = malloc(sizeof(huff_node_t));
+
         root->byte = malloc(sizeof(uint8_t));
-        *(uint8_t*)root->byte = '*';
+        *(uint8_t *)root->byte = '*';
+
         root->frequency = node->frequency;
         root->left = NULL;
         root->right = node;
+
         return root;
-    }else{
-        while(hq->size>1){
-            huff_node_t* left = huff_dequeue(hq);
-            huff_node_t* right = huff_dequeue(hq);
-            huff_node_t* root = malloc(sizeof(huff_node_t));
-            root->byte = malloc(sizeof(uint8_t));
-            *(uint8_t*)root->byte = '*';
-            root->frequency = left->frequency + right->frequency;
-            root->left = left;
-            root->right = right;
-            huff_enqueue(hq, root->byte, root->frequency, root->left, root->right);
+    }
+    else
+    {
+        while (hq->size > 1)
+        {
+            huff_node_t *left = huff_dequeue(hq);
+            huff_node_t *right = huff_dequeue(hq);
+
+            void *internal_byte = malloc(sizeof(uint8_t));
+            *(uint8_t *)internal_byte = '*';
+
+            uint64_t new_frequency = left->frequency + right->frequency;
+
+            huff_enqueue(hq, internal_byte, new_frequency, left, right);
         }
+
         return huff_dequeue(hq);
     }
 }
 
-// TODO: REMOVE PRINT TREE
-void huff_print_tree(huff_node_t *ht)
+void huff_write_tree_to_file(huff_node_t *ht, FILE *output)
 {
     if (ht != NULL)
     {
         if (is_special_node(ht))
         {
-            printf("%c", '\\');
+            char scape_char = '\\';
+            fwrite(&scape_char, sizeof(uint8_t), 1, output);
         }
- 
-        printf("%c", *(uint8_t *)ht->byte);
- 
-        huff_print_tree(ht->left);
-        huff_print_tree(ht->right);
+
+        fwrite((uint8_t *)ht->byte, sizeof(uint8_t), 1, output);
+
+        huff_write_tree_to_file(ht->left, output);
+        huff_write_tree_to_file(ht->right, output);
     }
 }
 
@@ -123,11 +131,14 @@ uint16_t huff_get_tree_size(huff_node_t *ht)
         return 0;
     }
     else
-    { 
-        if (is_special_node(ht)) {
+    {
+        if (is_special_node(ht))
+        {
             return 2 + huff_get_tree_size(ht->left) + huff_get_tree_size(ht->right);
-        } else {
+        }
+        else
+        {
             return 1 + huff_get_tree_size(ht->left) + huff_get_tree_size(ht->right);
-        } 
+        }
     }
 }
